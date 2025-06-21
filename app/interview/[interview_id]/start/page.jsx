bf8d4e89de9dddea66c5be00d2ccfaea82a46f97
Rohit Raj <rohitraj@Rohits-Mac-mini.local>
2025-06-21 19:51:17 +0530

    Add AlertConfirmation component and integrate with StartInterview; update package dependencies and integrated VAPI

--- app/interview/[interview_id]/start/page.jsx
+++ app/interview/[interview_id]/start/page.jsx
@@ -3,12 +3,110 @@ import { useUser } from "@/app/provider";
 import { InterviewDataContext } from "@/context/InterviewDataContext";
 import { Mic, Phone, Timer } from "lucide-react";
 import Image from "next/image";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import Vapi from "@vapi-ai/web";
+import AlertConfirmation from "./_components/AlertConfirmation";
+import { toast } from "sonner";
 
 function StartInterview() {
   const { interviewInfo, setInterviewInfo } = useContext(InterviewDataContext);
   const userContext = useUser();
   const user = userContext?.user;
+  const [activeUser, setActiveUser] = useState(false);
+  const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_PUBLIC_KEY);
+
+  useEffect(() => {
+    interviewInfo && startCall();
+  }, [interviewInfo]);
+
+  const startCall = () => {
+    let questionList;
+    interviewInfo?.interviewData?.questionList.forEach(
+      (item, index) => (questionList = item?.question + "," + questionList)
+    );
+    const assistantOptions = {
+      name: "AI Recruiter",
+      firstMessage:
+        "Hi " +
+        interviewInfo?.userName +
+        ", how are you? Ready for your interview on " +
+        interviewInfo?.interviewData?.jobPosition +
+        "?",
+      transcriber: {
+        provider: "deepgram",
+        model: "nova-2",
+        language: "en-US",
+      },
+      voice: {
+        provider: "vapi",
+        voiceId: "Neha",
+      },
+      model: {
+        provider: "openai",
+        model: "gpt-4",
+        messages: [
+          {
+            role: "system",
+            content:
+              `
+    You are an AI voice assistant conducting interviews.
+  Your job is to ask candidates provided interview questions, assess their responses.
+  Begin the conversation with a friendly introduction, setting a relaxed yet professional tone. Example:
+  "Hey there! Welcome to your ` +
+              interviewInfo?.interviewData?.jobPosition +
+              ` interview. Let’s get started with a few questions!"
+  Ask one question at a time and wait for the candidate’s response before proceeding. Keep the questions clear and concise. Below Are the questions ask one by one:
+  Questions: ` +
+              questionList +
+              `
+  If the candidate struggles, offer hints or rephrase the question without giving away the answer. Example:
+  "Need a hint? Think about how React tracks component updates!"
+  Provide brief, encouraging feedback after each answer. Example:
+  "Nice! That’s a solid answer."
+  "Hmm, not quite! Want to try again?"
+  Keep the conversation natural and engaging—use casual phrases like "Alright, next up..." or "Let’s tackle a tricky one!"
+  After 5-7 questions, wrap up the interview smoothly by summarizing their performance. Example:
+  "That was great! You handled some tough questions well. Keep sharpening your skills!"
+  End on a positive note:
+  "Thanks for chatting! Hope to see you crushing projects soon!"
+  Key Guidelines:
+  ✅ Be friendly, engaging, and witty 🎤
+  ✅ Keep responses short and natural, like a real conversation
+  ✅ Adapt based on the candidate’s confidence level
+  ✅ Ensure the interview remains focused on React
+  `.trim(),
+          },
+        ],
+      },
+    };
+
+    vapi.start(assistantOptions);
+  };
+
+  const stopInterview = () => {
+    console.log("Stopping interview...");
+    vapi.stop();
+  };
+
+  vapi.on("call-start", () => {
+    console.log("Voice conversation started");
+    toast("Call connected successfully");
+  });
+
+  vapi.on("speech-start", () => {
+    console.log("User started speaking");
+    setActiveUser(false);
+  });
+  vapi.on("speech-end", () => {
+    console.log("User stopped speaking");
+    setActiveUser(true);
+  });
+
+  vapi.on("call-end", () => {
+    console.log("Voice conversation ended");
+    toast("Interview ended successfully");
+  });
+
   return (
     <div className="p-20 lg:px-48 xl:px-56">
       <h2 className="font-bold text-xl flex justify-between">
@@ -20,29 +118,41 @@ function StartInterview() {
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-7 mt-5">
         <div className="bg-white h-[400px] rounded-lg border flex flex-col gap-3 items-center justify-center">
-          <Image
-            src={"/AiAvatar.png"}
-            alt="AI"
-            width={100}
-            height={100}
-            className="w-[60px] h-[60px] rounded-full object-cover"
-          />
+          <div className="relative">
+            {!activeUser && (
+              <span className="absolute inset-0 rounded-full bg-blue-500 opacity-75 animate-ping" />
+            )}
+            <Image
+              src={"/AiAvatar.png"}
+              alt="AI"
+              width={100}
+              height={100}
+              className="w-[60px] h-[60px] rounded-full object-cover"
+            />
+          </div>
           <h2>AI Recruiter</h2>
         </div>
         <div className="bg-white h-[400px] rounded-lg border flex flex-col gap-3 items-center justify-center">
-          <Image
-            src={user?.picture || "/user.png"}
-            alt="AI"
-            width={100}
-            height={100}
-            className="w-[60px] h-[60px] rounded-full object-cover"
-          />
+          <div className="relative">
+            {!activeUser && (
+              <span className="absolute inset-0 rounded-full bg-blue-500 opacity-75 animate-ping" />
+            )}
+            <Image
+              src={user?.picture || "/user.png"}
+              alt="AI"
+              width={100}
+              height={100}
+              className="w-[60px] h-[60px] rounded-full object-cover"
+            />
+          </div>
           <h2>{interviewInfo?.userName}</h2>
         </div>
       </div>
       <div className="flex justify-center items-center gap-5 mt-10">
         <Mic className="h-12 w-12 p-3 bg-gray-500 text-white rounded-full cursor-pointer" />
-        <Phone className="h-12 w-12 p-3 bg-red-500 text-white rounded-full cursor-pointer" />
+        <AlertConfirmation stopInterview={() => stopInterview()}>
+          <Phone className="h-12 w-12 p-3 bg-red-500 text-white rounded-full cursor-pointer" />
+        </AlertConfirmation>
       </div>
       <h2 className="text-sm text-gray-400 text-center mt-5">
         Interview is in Progress...
